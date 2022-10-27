a5cf02d0e26bdff448c347171ffd3c556360efcd
masgalih320 <masgalih320@gmail.com>
2022-10-27 18:36:30 +0800

    Update API endpoint for question bank

--- pages/api/question.js
+++ pages/api/question.js
@@ -1,3 +1,21 @@
+// Define API routes for getting question bank
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  return res.status(200).json({
+    status: 200,
+    msg: "success",
+    data: [
+      {
+        time: 10000,
+        question: "Siapakah presiden Negara Republik Indonesia ke-1?",
+        answer: "Ir. Soekarno",
+        choices: ["WR. Soepratman", "Soeharto", "Puan Maharani", "Ir. Soekarno"],
+      },
+      {
+        time: 5000,
+        question: "Kerusuhan Banjarmasin terjadi pada tahun berapa?",
+        answer: 1997,
+        choices: [1997, 2002, 2013, 1945],
+      },
+    ],
+  });
 }
