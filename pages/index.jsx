6e67aa2d42dcd167f9f9aa5e815d9668c502593e
masgalih320 <masgalih320@gmail.com>
2022-10-27 15:14:15 +0800

    Update

---
+++ pages/index.jsx
@@ -0,0 +1,23 @@
+export default function Home() {
+  return (
+    <div className="overflow-hidden">
+      <div className="bg-purple-400 h-[45vh] p-6 mb-4 flex items-center justify-center">
+        <div className="font-bold text-4xl select-none">Bebek bebek apa yang bikin kzl?</div>
+      </div>
+      <div className="h-[50vh] flex justify-between items-center space-x-5 mx-5">
+        <div className="p-4 bg-blue-400 border-blue-500 hover:bg-blue-600 hover:border-blue-600 duration-300 border-2 h-80 w-full rounded-lg cursor-pointer flex items-center justify-center">
+          <div className="text-white font-sans font-bold text-2xl text-center select-none">Bebek ga bisa jalan cuyy anjay mabar</div>
+        </div>
+        <div className="p-4 bg-blue-400 border-blue-500 hover:bg-blue-600 hover:border-blue-600 duration-300 border-2 h-80 w-full rounded-lg cursor-pointer flex items-center justify-center">
+          <div className="text-white font-sans font-bold text-2xl text-center select-none">Bebek</div>
+        </div>
+        <div className="p-4 bg-blue-400 border-blue-500 hover:bg-blue-600 hover:border-blue-600 duration-300 border-2 h-80 w-full rounded-lg cursor-pointer flex items-center justify-center">
+          <div className="text-white font-sans font-bold text-2xl text-center select-none">Bebek</div>
+        </div>
+        <div className="p-4 bg-blue-400 border-blue-500 hover:bg-blue-600 hover:border-blue-600 duration-300 border-2 h-80 w-full rounded-lg cursor-pointer flex items-center justify-center">
+          <div className="text-white font-sans font-bold text-2xl text-center select-none">Bebek</div>
+        </div>
+      </div>
+    </div>
+  )
+}
