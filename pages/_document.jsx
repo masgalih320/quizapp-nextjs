faa1db08c77050a18a9b90c3559fbae5c8641677
masgalih320 <masgalih320@gmail.com>
2022-10-27 18:27:38 +0800

    Add _document.jsx and add Question API Endpoint

---
+++ pages/_document.jsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>Simple Quiz App with NextJS</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
