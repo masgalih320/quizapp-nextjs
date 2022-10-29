a62819ca2d95cd3071b8a2972e6f286ce29debce
masgalih320 <masgalih320@gmail.com>
2022-10-29 10:04:25 +0800

    Restructured variable for efficiency and stucked on timer logical

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Home({ question }) {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
@@ -8,19 +8,29 @@ export default function Home({ question }) {
   const [isOver, setIsOver] = useState(false)
   const [timer, setTimer] = useState(100)
 
+  useEffect(() => {
+    let timerEl = document.getElementById('timer')
+    timerEl.style.width = `${timer}%`
+  })
+
   function handleClick(ans) {
     ans === currentQuestionAnswer ? setCorrectAnswerTotal(correctAnswerTotal + 1) : setIncorrectAnswerTotal(incorrectAnswerTotal + 1)
     setCurrentQuestionIndex(currentQuestionIndex + 1)
     setCurrentQuestionAnswer(question[question.length - 1].answer)
   }
 
-  setInterval(() => {
-    setTimer(timer - 10);
-  }, 1000)
+  function timerCountdown() {
+    setTimeout(() => {
+      setTimer((timer > 0) ? timer - 10 : 0)
+      timer == 0 ? setIsOver(true) : timerCountdown()
+    }, 1000)
+  }
+
+  timerCountdown()
 
   return (
     <div className="overflow-hidden select-none relative">
-      <div className="absolute top-0 h-2 z-50 bg-gray-300" style={{ width: `${timer}%` }} id="timer"></div>
+      <div className="absolute top-0 h-2 z-50 bg-gray-300 w-full" id="timer"></div>
       <div className="h-[45vh] p-6 mb-4 flex items-center justify-center relative">
         <div className="bg-gradient-to-b from-blue-500 to-white absolute top-0 left-0 right-0 bottom-0"></div>
         <h3 className="absolute top-2 left-3 z-50">
@@ -28,7 +38,7 @@ export default function Home({ question }) {
           <div className="font-bold">Correct: <span id="totalCorrect">{correctAnswerTotal}</span></div>
           <div className="font-bold">Incorrect: <span id="totalIncorrect">{incorrectAnswerTotal}</span></div>
         </h3>
-        <div className="font-bold text-4xl z-50 text-center">{question.length - 1 >= currentQuestionIndex ? question[currentQuestionIndex].question : ""}</div>
+        <div className="font-bold text-4xl z-50 text-center">{timer}{question.length - 1 >= currentQuestionIndex ? question[currentQuestionIndex].question : ""}</div>
       </div>
       <div className="h-[50vh] flex justify-between items-center space-x-5 mx-5">
         {question.length - 1 >= currentQuestionIndex ? question[currentQuestionIndex].choices.map((seg) => {
